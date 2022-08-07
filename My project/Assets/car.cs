5513abf45414dbb82c213ceaf8cc8cc039f05446
dana-kuwaitcodes <danahossam056@gmail.com>
2022-08-07 16:20:28 +0300

    cw2

---
+++ My project/Assets/car.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string hero_name = "dana";
+    int heroheight = 160;
+    int hero_age = 17;
+    string herosuperpower = "fly";
+    void Start()
+    {
+        print(hero_name);
+        print(heroheight);
+        print(hero_age);
+        print(herosuperpower);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
